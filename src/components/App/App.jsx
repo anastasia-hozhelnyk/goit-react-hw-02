eb37eb7cee88b7e0f40c7fac367a7f7c698369a4
Anastasia Hozhelnyk <anastasiaromanuk00@gmail.com>
2024-06-25 04:40:50 +0300

    finish

---
+++ src/components/App/App.jsx
@@ -0,0 +1,64 @@
+import css from "./App.module.css";
+
+import { useState, useEffect } from "react";
+import Description from "../Description/Description";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
+import Notification from "../Notification/Notification";
+
+const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const storedFeedback = JSON.parse(localStorage.getItem("feedback"));
+    return (
+      storedFeedback || {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      }
+    );
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback({
+      ...feedback,
+      [feedbackType]: feedback[feedbackType] + 1,
+    });
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  return (
+    <div className={css["container"]}>
+      <Description />
+      <Options
+        onUpdateFeedback={updateFeedback}
+        onResetFeedback={resetFeedback}
+        total={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          results={feedback}
+          total={totalFeedback}
+          positive={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+};
+
+export default App;
